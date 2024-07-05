1ea123b6e4c6c452cf22b6f3e647c1b618805d16
alingse <alingse@foxmail.com>
2024-07-06 00:25:06 +0800

    remove all

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/niaotuobang/niaotuobang-wiki
-
-go 1.18
-
-require github.com/graph-gophers/graphql-go v1.4.0
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/niaotuobang/niaotuobang-wiki/cmd"
-)
-
-func main() {
-	cmd.WebMain()
-}
